0b932b59be8f02d8e5900db1f92490674872632b
jmandevel <newjourneyman@yahoo.com>
2025-06-14 02:03:54 -0400

    Refactor symbol resolution
    Implement more features of resolving, including looking up symbols by identifier literal in same scope and reference type modifiers.

--- src/resolve_symbols.cpp
+++ src/resolve_symbols.cpp
@@ -5,14 +5,53 @@ namespace requite {
 bool Context::resolveSymbol(requite::Symbol &out_symbol, requite::Scope &scope,
                             requite::Expression &symbol_expression) {
   switch (const requite::Opcode opcode = symbol_expression.getOpcode()) {
+  case requite::Opcode::__IDENTIFIER_LITERAL: {
+    requite::RootSymbol user =
+        scope.lookupInternalRootSymbol(symbol_expression.getDataText());
+    if (user.getIsNone()) {
+      return false;
+    }
+    else if (user.getIsAlias()) {
+      requite::Alias &alias = user.getAlias();
+      if (alias.getSymbol().getIsEmpty()) {
+        if (!this->prototypeUserSymbol(alias)) {
+          return false;
+        }
+      }
+      out_symbol.wrapSymbol(alias.getSymbol());
+    } else if (user.getIsObject()) {
+      requite::Object &object = user.getObject();
+      out_symbol.getRoot().setType(requite::RootSymbolType::OBJECT);
+      out_symbol.getRoot().setObject(object);
+    }
+    return true;
+  }
+  case requite::Opcode::_ASCRIBE_FIRST_BRANCH: {
+    requite::Expression &ascribed = symbol_expression.getBranch();
+    requite::MakeAttributesResult result =
+        requite::Attributes::makeAttributes(*this, ascribed.getNext());
+    if (result.has_error) {
+      return false;
+    }
+    out_symbol.applyAttributes(result.attributes);
+    return this->resolveSymbol(out_symbol, scope, ascribed);
+  }
   case requite::Opcode::SIGNED_INTEGER: {
     out_symbol.getRoot().setType(requite::RootSymbolType::SIGNED_INTEGER);
     unsigned depth;
-    if (!this->evaluateConstantUnsigned(depth, scope, symbol_expression.getBranch())) {
-        return false;
+    if (!this->evaluateConstantUnsigned(depth, scope,
+                                        symbol_expression.getBranch())) {
+      return false;
     }
     out_symbol.getRoot().setDepth(depth);
-  } return true;
+    return true;
+  }
+  case requite::Opcode::_REFERENCE: {
+    requite::SubSymbol &sub = out_symbol.makeSubSymbol();
+    sub.setType(requite::SubSymbolType::REFERENCE);
+    return this->resolveSymbol(out_symbol, scope,
+                               symbol_expression.getBranch());
+  }
   default:
     break;
   }
