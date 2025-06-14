42f380e4b6d41e9210c9463e7dbbc21c7923dde6
jmandevel <newjourneyman@yahoo.com>
2025-06-14 01:53:15 -0400

    Refactor evaluation of numeric integer literal values

--- src/evaluate_values.cpp
+++ src/evaluate_values.cpp
@@ -1,11 +1,16 @@
 #include <requite/context.hpp>
+#include <requite/numeric.hpp>
 
 namespace requite {
 
 bool Context::evaluateConstantUnsigned(unsigned& out_unsigned, requite::Scope& scope, requite::Expression &value_expression) {
     switch (const requite::Opcode opcode = value_expression.getOpcode()) {
         case requite::Opcode::__INTEGER_LITERAL: {
-            out_unsigned = 32;
+            requite::NumericResult result =
+                requite::getNumericValue(value_expression.getSourceText(), out_unsigned);
+            if (result != requite::NumericResult::OK) {
+                return false;
+            }
         } return true;      
         case requite::Opcode::ADDRESS_DEPTH: {
             out_unsigned = this->getAddressDepth();
