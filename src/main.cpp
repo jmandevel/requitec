68945b2b2ddd09dbc3bf2384c9fe8902ebb23245
jmandevel <newjourneyman@yahoo.com>
2025-07-01 14:27:40 -0400

    Remove fake argv

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,7 @@
 int main(int argc, const char **argv) {
   std::string executable_path =
       llvm::sys::fs::getMainExecutable(argv[0], reinterpret_cast<void *>(main));
-  const char* fake_argv[] = {
-    "./requite", "--emit=ir", "test.rq", "-o", "test.ir"
-  };
-  llvm::cl::ParseCommandLineOptions(5, fake_argv);
+  llvm::cl::ParseCommandLineOptions(argc, argv);
   requite::Context context(std::move(executable_path));
   if (!context.run()) {
     return 1;
