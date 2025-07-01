951be03e04272ab6ec9ba69d42ba831fb4b227d4
jmandevel <newjourneyman@yahoo.com>
2025-07-01 14:38:32 -0400

    Add fake argv for devops purposes
    This will be removed later... makes it really easy to run and debug in ide but also allows for running makefiles for other testing.

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,20 @@
 #include <requite/context.hpp>
 #include <requite/options.hpp>
 
-#include <llvm/Support/FileSystem.h>
 #include <llvm/Support/CommandLine.h>
+#include <llvm/Support/FileSystem.h>
 
 int main(int argc, const char **argv) {
   std::string executable_path =
       llvm::sys::fs::getMainExecutable(argv[0], reinterpret_cast<void *>(main));
-  llvm::cl::ParseCommandLineOptions(argc, argv);
+  // TODO remove fake_argv when done with alpha stage of development. this is here for ease of use.
+  const char *fake_argv[] = {"./requite", "--emit=ir", "test.rq", "-o",
+                             "test.ir"};
+  if (argc == 1) {
+    llvm::cl::ParseCommandLineOptions(5, fake_argv);
+  } else {
+    llvm::cl::ParseCommandLineOptions(argc, argv);
+  }
   requite::Context context(std::move(executable_path));
   if (!context.run()) {
     return 1;
