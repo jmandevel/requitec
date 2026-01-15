2770b852da8f1869beda95619844047dbe107b81
jmandevel <newjourneyman@yahoo.com>
2026-01-15 10:08:27 -0500

    Add steps left to see
    * this will prevent symbolic execution from going too far if infinite loop occurs etc
    * clang does something simiilar

--- include/rq/see.hpp
+++ include/rq/see.hpp
@@ -9,6 +9,26 @@ namespace rq {
 struct SymbolicExecutionEngine final {
   using Self = rq::SymbolicExecutionEngine;
 
+  std::int64_t _initial_steps_left;
+  std::int64_t _steps_left;
+
+  void _resetStepsLeft() {
+    this->_steps_left = this->_initial_steps_left;
+  }
+  [[nodiscard]] RQ_ALWAYS_INLINE std::int64_t getStepsLeft() const {
+    return this->_steps_left;
+  }
+  [[nodiscard]] RQ_ALWAYS_INLINE std::int64_t getInitialStepsLeft() const {
+    return this->_initial_steps_left;
+  }
+  [[nodiscard]] RQ_ALWAYS_INLINE bool getHasStepsLeft() const {
+    return this->_steps_left > 0;
+  }
+  [[nodiscard]] RQ_ALWAYS_INLINE bool _decrementStep() {
+    RQ_ASSERT(this->getHasStepsLeft(), "no steps left");
+    this->_steps_left--;
+    return this->getHasStepsLeft();
+  }
   [[nodiscard]] llvm::StringRef evaluateName(rq::Expression &expression);
 };
 
