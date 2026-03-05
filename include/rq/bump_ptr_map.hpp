28a42f251b9bcc57d038408425f5287fe65c8ef1
Dan Valcour <newjourneyman@yahoo.com>
2026-03-04 22:46:44 -0500

    Add BumpPtrMap
    TODO implement most of it...

---
+++ include/rq/bump_ptr_map.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <functional>
+#include <cstddef>
+
+namespace rq {
+
+// A map backed by a binary search tree ordered by std::hash of the key.
+// Nodes are allocated using a llvm::BumpPtrAllocator.
+
+// TODO implement as a red-black tree
+
+template <typename KeyParam, typename ValueParam, typename HashParam = std::hash<KeyParam>>
+struct BumpPtrMapNode {
+    using Key = KeyParam;
+    using Value = ValueParam;
+    using Hash = HashParam;
+    using Self = rq::BumpPtrMap<Key, Value, Hash>;
+
+    Key key;
+    Value value;
+    Hash hash;
+    Self *left;
+    Self *right;
+};
+
+template <typename KeyParam, typename ValueParam, typename HashParam = std::hash<KeyParam>>
+struct BumpPtrMap {
+  using Key = KeyParam;
+  using Value = ValueParam;
+  using Hash = HashParam;
+  using Self = rq::BumpPtrMap<Key, Value, Hash>;
+  using Node = rq::BumpPtrMapNode<Key, Value, Hash>;
+  using KeyType = Key;
+  using ValueType = Value;
+  using HashType = Hash;
+  using HashValueType = std::size_t;
+
+  Node *_root{nullptr};
+  std::size_t _count{0};
+
+  BumpPtrMap() = default;
+  BumpPtrMap() = delete;
+  BumpPtrMap(const BumpPtrMap &) = delete;
+  BumpPtrMap(BumpPtrMap &&) = delete;
+  BumpPtrMap &operator=(const BumpPtrMap &) = delete;
+  BumpPtrMap &operator=(BumpPtrMap &&) = delete;
+  ~BumpPtrMap() = default;
+
+  std::size_t getCount() const { return _count; }
+  bool getIsEmpty() const { return _count == 0; }
+
+  // TODO implement rest
+};
+
+} // namespace rq
