80136ddcaf3676ff2a86d5245107745c0d5dc951
Dan Valcour <newjourneyman@yahoo.com>
2026-06-28 12:43:36 -0400

    Fix codeunit name

--- include/rq/codeunits.hpp
+++ include/rq/codeunits.hpp
@@ -1562,7 +1562,7 @@ getCsvQuotedValueText(char codeunit) {
   case 0xE9:
     return "\\E9\\";
   case 0xEA:
-    return "\\LA\\";
+    return "\\EA\\";
   case 0xEB:
     return "\\EB\\";
   case 0xEC:
